ed7ed7e06389cae26ef4fcc628e4c0ac09caf74b
Anne Marie Charmaine Mendoza <anne.marie.c.mendoza@accenture.com>
2026-05-20 04:54:44 +0000

    added mock server and created table, completed activity

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -4,7 +4,27 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("sapips.training.odata.controller.View", {
-        onInit() {
+        onInit() {},
+        
+        onAfterRendering: function () {
+            let oModel = this.getView().getModel();
+            let oTable = this.byId("productTable");
+
+            oModel.read("/Products", {
+                success: function (oData) {
+                    //sort the data by ProductName
+                    let oBinding = oTable.getBinding("items");
+                    oBinding.sort(new sap.ui.model.Sorter("ProductName"));
+
+                    //filter the data to exclude discontinued products
+                    oBinding.filter(new sap.ui.model.Filter("Discontinued", sap.ui.model.FilterOperator.EQ, false));
+                    console.log("Success", oData);
+                },
+                error: function (oError) {
+                    console.error("Error", oError);
+                }
+            });
         }
+
     });
 });
